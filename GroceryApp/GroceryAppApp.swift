02a42008ba3cb1a662650350b988b627f16831a0
Yash Shah <62358049+git-yash@users.noreply.github.com>
2024-11-16 13:41:06 -0600

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Yash Shah on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
