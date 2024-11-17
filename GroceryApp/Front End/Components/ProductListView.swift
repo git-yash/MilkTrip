26063a934b6dc6120bf6c09dae1e88fb8a6a22ba
Yash Shah <62358049+git-yash@users.noreply.github.com>
2024-11-16 20:03:21 -0600

    ProductListView background tweak

--- GroceryApp/Front End/Components/ProductListView.swift
+++ GroceryApp/Front End/Components/ProductListView.swift
@@ -1,19 +1,13 @@
-//
-//  ProductListView.swift
-//  GroceryApp
-//
-//  Created by Aiden Seibel on 11/16/24.
-//
-
 import SwiftUI
 
 struct ProductListView: View {
     var product: Product
-    
+
     var screenWidth: Double = UIScreen.main.bounds.width
 
     var body: some View {
-        HStack{
+        HStack {
+            // Image Section
             if let url = URL(string: product.image_url) {
                 AsyncImage(url: url) { phase in
                     switch phase {
@@ -39,25 +33,28 @@ struct ProductListView: View {
                 // If the URL is invalid, show a placeholder
                 Color.gray
                     .frame(width: 50, height: 50)
+                    .cornerRadius(5)
             }
-            
-            VStack(alignment: .leading, spacing: 2){
+
+            // Product Info Section
+            VStack(alignment: .leading, spacing: 2) {
                 Text(product.name)
                     .bold()
-                
-                HStack(alignment: .center, spacing: 5){
+
+                HStack(alignment: .center, spacing: 5) {
                     Text("from \(product.getBrand())")
                         .font(.system(size: 12))
-                    Circle().frame(width: 2, height: 2)
+                    Circle()
+                        .frame(width: 2, height: 2)
                     Text(product.brand)
                         .font(.system(size: 12))
                 }
-                
             }
 
             Spacer()
-            
-            VStack(alignment: .leading){
+
+            // Price Section
+            VStack(alignment: .leading) {
                 Text("$" + String(format: "%.2f", product.getMostRecentPrice() ?? 0.00))
                     .font(.system(size: 18))
                     .bold()
@@ -65,8 +62,10 @@ struct ProductListView: View {
         }
         .frame(height: screenWidth * 0.10)
         .padding(EdgeInsets(top: 12, leading: 10, bottom: 12, trailing: 10))
-        .cornerRadius(10)
-        .overlay(RoundedRectangle(cornerRadius: 10).stroke(.gray, lineWidth: 1))
+        .background(
+            RoundedRectangle(cornerRadius: 10) // Apply rounded rectangle
+                .fill(Color(hex: "#393e46")) // Background color
+        )
     }
 }
 
