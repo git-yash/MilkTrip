5a87469c5104beef6522d16192d7a396a53097d4
aidenseibel <aiden.seibel@gmail.com>
2024-11-17 09:18:29 -0600

    uses api key now

--- GroceryApp/Back End/PinataService.swift
+++ GroceryApp/Back End/PinataService.swift
@@ -9,39 +9,54 @@ import Foundation
 import SwiftUI
 
 class PinataService {
+    // Replace this with your actual Pinata API key
+
     static func fetchFile(from cid: String, completion: @escaping (Data?) -> Void) {
-        let urlString = "https://gateway.pinata.cloud/ipfs/\(cid)"
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL.")
-            completion(nil)
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("Error fetching file: \(error.localizedDescription)")
-                completion(nil)
-                return
-            }
-
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                print("Invalid response from server.")
-                completion(nil)
-                return
-            }
-            
-            if let data = data {
-                if let responseString = String(data: data, encoding: .utf8) {
-                    print("Response from pinata:: "+responseString)
-                    completion(data)
-
-                } else {
-                    print("Unable to retrieve response from pinata.")
+        if let filePath = Bundle.main.path(forResource: "PinataKey", ofType: "txt"){
+            do {
+                let api_key = try String(contentsOfFile: filePath, encoding: .utf8).trimmingCharacters(in: .whitespacesAndNewlines)
+                
+                let urlString = "https://gateway.pinata.cloud/ipfs/\(cid)"
+                guard let url = URL(string: urlString) else {
+                    print("Invalid URL.")
+                    completion(nil)
+                    return
                 }
+                
+                var request = URLRequest(url: url)
+                request.httpMethod = "GET"
+                
+                // Set the API key in the Authorization header
+                request.setValue("Bearer \(api_key)", forHTTPHeaderField: "Authorization")
+                
+                URLSession.shared.dataTask(with: request) { data, response, error in
+                    if let error = error {
+                        print("Error fetching file: \(error.localizedDescription)")
+                        completion(nil)
+                        return
+                    }
+                    
+                    guard let httpResponse = response as? HTTPURLResponse,
+                          (200...299).contains(httpResponse.statusCode) else {
+                        print("Invalid response from server.")
+                        completion(nil)
+                        return
+                    }
+                    
+                    if let data = data {
+                        if let responseString = String(data: data, encoding: .utf8) {
+                            print("Response from Pinata: \(responseString)")
+                            completion(data)
+                        } else {
+                            print("Unable to retrieve response from Pinata.")
+                            completion(nil)
+                        }
+                    }
+                }.resume()
             }
-
-
-        }.resume()
+            catch{
+                print("error in getting api key")
+            }
+        }
     }
 }
